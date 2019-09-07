687509a77584200250f88ca28f7e789713c066c7
mahmudy9 <mhmudee@gmail.com>
2019-09-07 12:42:43 +0200

    solved delay issue by php artisan queue:work --sleep=0

--- app/Events/ErrorEvent.php
+++ app/Events/ErrorEvent.php
@@ -9,8 +9,9 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class ErrorEvent
+class ErrorEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- public/js/app.js
+++ public/js/app.js
@@ -63076,8 +63076,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/mahmud/code/WebSockets/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/mahmud/code/WebSockets/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/mahmud/code/websockets/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/mahmud/code/websockets/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
